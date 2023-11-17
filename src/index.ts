ddc381c1c8ccb926556c9d962961d02957bd2baf
Carla Silva <carla.silva@movestock.com>
2023-11-16 21:15:36 -0300

    added new code

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,144 @@
-// class Carro {
-//   marca: string
-//   modelo: string
-//   ano: number
+// class Animal {
+//   constructor(
+//     public nome: string,
+//     public tipo: string
+//   ) { }
 
-//   constructor(marca: string, modelo: string, ano: number) {
-//     this.marca = marca
-//     this.modelo = modelo
-//     this.ano = ano
+//   emitirSom() {
+//     console.log(`${this.nome} emitiu som.`)
 //   }
-
-//   exibirInformacoes(): void {
-//     console.log(`Este é um ${this.marca} ${this.modelo} do ano ${this.ano}`)
-//   }
-// }
-
-// const meuCarro = new Carro('Toyota', 'Corolla', 2022)
-
-// console.log(meuCarro)
-// meuCarro.exibirInformacoes()
-
-// class Exemplo {
-//   constructor(public variavelPublica: string, private variavelPrivada: number) { }
-
-//   mostrarDetalhes(): void {
-//     console.log(`Variável Pública: ${this.variavelPublica}`)
-//     console.log(`Variável Privada: ${this.variavelPrivada}`)
-//   }
-// }
-
-// const instancia = new Exemplo('Olá', 40)
-// console.log(instancia.variavelPublica)
-// console.log(instancia.variavelPrivada)
-
-// instancia.mostrarDetalhes()
-
-class ContaBancaria {
-  constructor(private _valor: number) { }
-
-  get valor(): number {
-    return this._valor
-  }
-
-  set valor(novoValor: number) {
-    if (novoValor >= 0) {
-      this._valor = novoValor * 2
-    } else {
-      console.log('O valor não pode ser negativo.')
-    }
-  }
-}
-
-const conta = new ContaBancaria(10)
-console.log(conta.valor = 30)
-
-// conta.valor = 20
-
-console.log(conta.valor)
-
-// conta.valor = -4
-// console.log(conta.valor)
-
-// abstract class Animal {
-//   constructor(protected nome: string, protected tipo: string) { }
-
-//   // emitirSom(): void {
-//   //   console.log("Som genérico emitido")
-//   // }
-
-//   abstract emitirSom(): void
 // }
 
 // class Cachorro extends Animal {
-//   constructor(nome: string, private raca: string) {
+//   constructor(
+//     nome: string,
+//     private raca: string
+//   ) {
 //     super(nome, "Cachorro")
 //   }
 
-//   emitirSom(): void {
-//     console.log("Au, au!")
+//   latir() {
+//     console.log(`${this.nome} (${this.raca} está latindo.)`)
 //   }
 // }
 
-// class Galinha extends Animal {
-//   constructor(nome: string, private corPena: string) {
-//     super(nome, 'Galinha')
-//   }
+// const meuCachorro = new Cachorro("Rex", "Labrador")
 
-//   emitirSom(): void {
-//     console.log('Cocoricó!')
+// meuCachorro.emitirSom()
+// meuCachorro.latir()
+// console.log(meuCachorro)
+
+// class Aluno {
+//   constructor(
+//     private nome: string,
+//     private idade: number
+//   ) { }
+
+//   apresentar() {
+//     console.log(`Aluno ${this.nome}, Idade: ${this.idade}`)
 //   }
 // }
 
-// // const animal = new Animal("Mimoso", "Gato")
-// // console.log(animal)
+// class Turma {
+//   constructor(
+//     private nome: string,
+//     private alunos: Aluno[] = []
+//   ) { }
 
-// const cachorro = new Cachorro("Totó", "Poodle")
-// console.log(cachorro)
-// cachorro.emitirSom()
-// // cachorro.latir()
+//   adicionarAluno(aluno: Aluno) {
+//     this.alunos.push(aluno)
+//   }
 
-// const galinha = new Galinha("Pintadinha", "Azul")
-// console.log(galinha)
-// galinha.emitirSom()
-// // galinha.cacarejar()
+//   listarAlunos() {
+//     console.log(`Alunos da turma ${this.nome}`)
+//     this.alunos.forEach(aluno => {
+//       aluno.apresentar()
+//     })
+//   }
+// }
+
+// const aluno1 = new Aluno('Monã', 20)
+// const aluno2 = new Aluno('Gabriel', 21)
+
+// const turmaA = new Turma('Turma 19 FullStack')
+
+// turmaA.adicionarAluno(aluno1)
+// turmaA.adicionarAluno(aluno2)
+
+// console.log(turmaA)
+// turmaA.listarAlunos()
+
+// class Lutador {
+//   constructor(
+//     public nome: string,
+//     public estilo: string
+//   ) { }
+
+//   treinar() {
+//     console.log(`${this.nome} está treinando.`)
+//   }
+// }
+
+// class Luta {
+//   constructor(
+//     public lutador1: Lutador,
+//     public lutador2: Lutador
+//   ) { }
+
+//   iniciarLuta() {
+//     console.log(`Começou a luta entre ${this.lutador1.nome} e ${this.lutador2.nome}!`)
+//   }
+// }
+
+// const lutador1 = new Lutador("Pablo", "Karatê")
+// const lutador2 = new Lutador("Marcos", "Karatê")
+
+// lutador1.treinar()
+// lutador2.treinar()
+
+// const luta = new Luta(lutador1, lutador2)
+
+// console.log(luta)
+
+// luta.iniciarLuta()
 
 
+// class Monitor {
+//   constructor(public nome: string) { }
 
+//   supervisionar() {
+//     console.log(`${this.nome} está monitorando a turma!`)
+//   }
+// }
 
+// class Turma {
+//   constructor(
+//     public nome: string,
+//     public monitores: Monitor[] = []
+//   ) { }
 
+//   adicionarMonitor(monitor: Monitor) {
+//     this.monitores.push(monitor)
+//   }
+
+//   mostrarMonitores() {
+//     if (this.monitores.length === 0) {
+//       console.log(`A turma ${this.nome} não possui monitores.`)
+//     } else {
+//       console.log(`Monitores da turma ${this.nome}:`)
+//       this.monitores.forEach(monitor => {
+//         console.log(monitor.nome)
+//       })
+//     }
+//   }
+// }
+
+// const monitorA = new Monitor("Carla")
+// const monitorB = new Monitor("Jean")
+
+// const turmaA = new Turma('Turma 16 fullstack')
+// turmaA.adicionarMonitor(monitorA)
+// turmaA.adicionarMonitor(monitorB)
+
+// console.log(turmaA)
 
